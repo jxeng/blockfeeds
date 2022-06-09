cd7184f49692b7521e6572da2f0994d937cd8dc7
jiangxuefeng <xf.jiang@sohu.com>
2022-06-09 18:58:41 +0800

    update

--- _next/static/bGzLWKUOs5hCYcDc_YCjj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/856-8d3af6c588bd5024.js","static/chunks/pages/index-19c0f30a16e94b55.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
